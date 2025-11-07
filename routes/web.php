11dfef97ccad2dd3d6bc53b3e0c5b0f275116d43
Skyshar96 <skyshar96@gmail.com>
2025-11-07 21:33:25 +0100

    portfolio php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', [PortfolioController::class, 'home'])->name('home');
 Route::get('/veille', [PortfolioController::class, 'veille'])->name('veille');
 Route::get('/stages', [PortfolioController::class, 'stages'])->name('stages');
 Route::get('/code-conduite', [PortfolioController::class, 'codeConduite'])->name('code-conduite');
+
+?>
